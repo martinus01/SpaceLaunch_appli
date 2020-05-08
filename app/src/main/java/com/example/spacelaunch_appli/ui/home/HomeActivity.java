938c9ece234e3cde81a28325288854347d08fa9b
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-08 19:20:13 +0200

    [features][page] ajout des page

---
+++ app/src/main/java/com/example/spacelaunch_appli/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.spacelaunch_appli.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spacelaunch_appli.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/spacelaunch_appli/ui/vehicules/VehiclesActivity.java
@@ -0,0 +1,16 @@
+package com.example.spacelaunch_appli.ui.vehicules;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spacelaunch_appli.R;
+
+public class VehiclesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicles);
+    }
+}
