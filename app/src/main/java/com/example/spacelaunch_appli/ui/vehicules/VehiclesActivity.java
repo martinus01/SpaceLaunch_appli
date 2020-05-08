c947da4443f1feed44deb2e05f08a8e5710e61f1
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-08 22:31:37 +0200

    [features][page] ajout des page

--- app/src/main/java/com/example/spacelaunch_appli/ui/vehicules/VehiclesActivity.java
+++ app/src/main/java/com/example/spacelaunch_appli/ui/vehicules/VehiclesActivity.java
@@ -12,5 +12,6 @@ public class VehiclesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicles);
+
     }
 }
