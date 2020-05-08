cca78b0309ef96dc28ca19b865d73f545fd3f1f1
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-08 17:04:45 +0200

    [features][SplashScreen] Progress Bar (3seconde)

--- app/src/main/java/com/example/spacelaunch_appli/SplashScreenActivity.java
+++ app/src/main/java/com/example/spacelaunch_appli/SplashScreenActivity.java
@@ -2,14 +2,31 @@ package com.example.spacelaunch_appli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-
+    private final int SPLASH_SCREEN_DELAY = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // rediriger ver sla page main_activity après 3 secondes.
+        Runnable runnable= new Runnable() {
+            @Override
+            public void run() {
+                //demarer la page
+                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // handler post delayed
+        new Handler().postDelayed(runnable,SPLASH_SCREEN_DELAY);
+
     }
 }
