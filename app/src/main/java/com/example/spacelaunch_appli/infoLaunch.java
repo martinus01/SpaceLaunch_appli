4112eaf2b8f858b1f701cf5bc24bec8eb0ff3542
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-11 14:24:04 +0200

    [features][PAGE] mise ne parenthèse du menu , contration sur la RECYCLER VIEW et info

--- app/src/main/java/com/example/spacelaunch_appli/infoLaunch.java
+++ app/src/main/java/com/example/spacelaunch_appli/infoLaunch.java
@@ -10,5 +10,6 @@ public class infoLaunch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_launch);
+
     }
 }
