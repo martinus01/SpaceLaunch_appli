002df1634f3998be732f8c887fe3c5036dc736af
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-18 23:51:50 +0200

    [develop]ajout de l'icone fin

--- app/src/main/java/com/example/spacelaunch_appli/LaunchActivity.java
+++ app/src/main/java/com/example/spacelaunch_appli/LaunchActivity.java
@@ -34,6 +34,7 @@ public class LaunchActivity extends AppCompatActivity {
     private Gson gson;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
